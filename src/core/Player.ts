c0e421d217d190c20c84e763fc9e1ec5d60d510a
Navaneeth M <navaneethna.m@gmail.com>
2025-04-17 17:09:15 +0530

    onBuffering, onTimeUpdate, setVolume(), .mute() events

--- src/core/Player.ts
+++ src/core/Player.ts
@@ -5,98 +5,121 @@ import { SegmentTemplateInfo } from '../dash/types.js';
 
 type PlayerEventHook = () => void;
 type ErrorHook = (err: unknown) => void;
+type TimeUpdateHook = (time: number) => void;
 
 export class Player {
-  private videoElement?: HTMLVideoElement;
-  private mediaSourceHandler?: MediaSourceHandler;
-  private manifestUrl?: string;
+    private videoElement?: HTMLVideoElement;
+    private mediaSourceHandler?: MediaSourceHandler;
+    private manifestUrl?: string;
 
-  public onPlay?: PlayerEventHook;
-  public onPause?: PlayerEventHook;
-  public onEnded?: PlayerEventHook;
-  public onReady?: PlayerEventHook;
-  public onError?: ErrorHook;
+    public onPlay?: PlayerEventHook;
+    public onPause?: PlayerEventHook;
+    public onEnded?: PlayerEventHook;
+    public onReady?: PlayerEventHook;
+    public onError?: ErrorHook;
+    public onTimeUpdate?: TimeUpdateHook;
+    public onBuffering?: PlayerEventHook;
 
-  constructor() {
-    logger.setLevel('debug');
-  }
-
-  attachVideoElement(video: HTMLVideoElement) {
-    this.videoElement = video;
-
-    this.videoElement.onplay = () => this.onPlay?.();
-    this.videoElement.onpause = () => this.onPause?.();
-    this.videoElement.onended = () => this.onEnded?.();
-    this.videoElement.onerror = () => this.onError?.(this.videoElement?.error);
-    logger.info('[Player] Video element attached');
-  }
-
-  async load(manifestUrl: string) {
-    if (!this.videoElement) {
-      throw new Error('[Player] No video element attached. Use attachVideoElement().');
+    constructor() {
+        logger.setLevel('debug');
     }
 
-    this.manifestUrl = manifestUrl;
-    logger.info('[Player] Loading manifest:', manifestUrl);
+    attachVideoElement(video: HTMLVideoElement) {
+        this.videoElement = video;
 
-    try {
-      const parser = new MPDParser();
-      const videoTracks = await parser.parseVideo(manifestUrl);
-      const audioTracks = await parser.parseAudio(manifestUrl);
-      const tracks: SegmentTemplateInfo[] = [...videoTracks, ...audioTracks];
+        video.onplay = () => this.onPlay?.();
+        video.onpause = () => this.onPause?.();
+        video.onended = () => this.onEnded?.();
+        video.onerror = () => this.onError?.(video.error);
+        video.ontimeupdate = () => this.onTimeUpdate?.(video.currentTime);
+        video.onwaiting = () => this.onBuffering?.();
 
-      this.mediaSourceHandler = new MediaSourceHandler(this.videoElement, tracks);
-      await this.mediaSourceHandler.init();
-
-      this.onReady?.();
-    } catch (err) {
-      logger.error('[Player] Error during load:', err);
-      this.onError?.(err);
-    }
-  }
-
-  play() {
-    this.videoElement?.play().catch(err => {
-      logger.error('[Player] Play error:', err);
-      this.onError?.(err);
-    });
-  }
-
-  pause() {
-    this.videoElement?.pause();
-  }
-
-  isPaused(): boolean {
-    return this.videoElement?.paused ?? true;
-  }
-
-  getCurrentTime(): number {
-    return this.videoElement?.currentTime ?? 0;
-  }
-
-  seekTo(time: number) {
-    if (this.videoElement) {
-      this.videoElement.currentTime = time;
-    }
-  }
-
-  reset() {
-    if (this.videoElement) {
-      this.videoElement.pause();
-      this.videoElement.removeAttribute('src');
-      this.videoElement.load();
+        logger.info('[Player] Video element attached');
     }
 
-    this.mediaSourceHandler = undefined;
-    this.manifestUrl = undefined;
+    async load(manifestUrl: string) {
+        if (!this.videoElement) {
+            throw new Error('[Player] No video element attached. Use attachVideoElement().');
+        }
 
-    logger.info('[Player] Reset complete');
-  }
+        this.manifestUrl = manifestUrl;
+        logger.info('[Player] Loading manifest:', manifestUrl);
 
-  destroy() {
-    this.reset();
-    this.videoElement = undefined;
+        try {
+            const parser = new MPDParser();
+            const videoTracks = await parser.parseVideo(manifestUrl);
+            const audioTracks = await parser.parseAudio(manifestUrl);
+            const tracks: SegmentTemplateInfo[] = [...videoTracks, ...audioTracks];
 
-    logger.info('[Player] Destroyed player');
-  }
+            this.mediaSourceHandler = new MediaSourceHandler(this.videoElement, tracks);
+            await this.mediaSourceHandler.init();
+
+            this.onReady?.();
+        } catch (err) {
+            logger.error('[Player] Error during load:', err);
+            this.onError?.(err);
+        }
+    }
+
+    play() {
+        this.videoElement?.play().catch(err => {
+            logger.error('[Player] Play error:', err);
+            this.onError?.(err);
+        });
+    }
+
+    pause() {
+        this.videoElement?.pause();
+    }
+
+    isPaused(): boolean {
+        return this.videoElement?.paused ?? true;
+    }
+
+    getCurrentTime(): number {
+        return this.videoElement?.currentTime ?? 0;
+    }
+
+    seekTo(time: number) {
+        if (this.videoElement) {
+            this.videoElement.currentTime = time;
+        }
+    }
+
+    setVolume(volume: number) {
+        if (this.videoElement) {
+            this.videoElement.volume = Math.max(0, Math.min(1, volume));
+        }
+    }
+
+    mute() {
+        if (this.videoElement) {
+            this.videoElement.muted = true;
+        }
+    }
+
+    unmute() {
+        if (this.videoElement) {
+            this.videoElement.muted = false;
+        }
+    }
+
+    reset() {
+        if (this.videoElement) {
+            this.videoElement.pause();
+            this.videoElement.removeAttribute('src');
+            this.videoElement.load();
+        }
+
+        this.mediaSourceHandler = undefined;
+        this.manifestUrl = undefined;
+
+        logger.info('[Player] Reset complete');
+    }
+
+    destroy() {
+        this.reset();
+        this.videoElement = undefined;
+        logger.info('[Player] Destroyed player');
+    }
 }
