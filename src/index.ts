82e14540e67868782502fdf9c0a61bfac39b6076
Navaneeth M <navaneethna.m@gmail.com>
2025-04-17 14:31:18 +0530

    Sample code

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+
+console.log("loaded")
+
+document.addEventListener('DOMContentLoaded', async () => {
+  const videoElement = document.getElementById('videoPlayer') as HTMLVideoElement;  // Use a real‑life free DASH manifest URL from Shaka demo assets.
+
+});
