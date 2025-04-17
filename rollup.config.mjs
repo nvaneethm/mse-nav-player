8f81f880afb920614cbccf23190b103faa2ccc95
Navaneeth M <navaneethna.m@gmail.com>
2025-04-17 17:47:20 +0530

    minify export code

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,13 +2,14 @@ import babel from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
-
+import terser from '@rollup/plugin-terser';
 const extensions = ['.ts', '.js'];
 
 const plugins = [
   resolve(),
   commonjs(),
   typescript({ tsconfig: './tsconfig.json' }),
+  terser()
 ];
 
 export default [
